903fadc155037d5724be2a20ba131b6a5df4d022
BackGyeHoon <bk88888@naver.com>
2023-03-23 09:51:35 +0900

    배포 레포명 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,48 +1,44 @@
 export default {
-  target: 'static',
+  target: "static",
   router: {
-    base: '/app/'
+    base: "/lucky-draw/",
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lucky-draw',
+    title: "lucky-draw",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content:
+          "initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width",
+      },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+  css: ["element-ui/lib/theme-chalk/index.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+  plugins: ["@/plugins/element-ui"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
-  }
-}
+  },
+};
