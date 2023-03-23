226b2effa85e9c96aa1df9bb291ce5c1a2ebf1ee
BackGyeHoon <bk88888@naver.com>
2023-03-23 09:45:23 +0900

    build test

--- pages/play.vue
+++ pages/play.vue
@@ -6,26 +6,48 @@
       </header>
       <div class="body2">
         <div class="search">
-          <el-input
-            placeholder="이름 입력"
-            v-model="input1" />
-          <el-button type="primary" @click="invite()" @keydown.enter="invite()">등록</el-button>
+          <el-input placeholder="이름 입력" v-model="input1" />
+          <el-button type="primary" @click="invite()" @keydown.enter="invite()"
+            >등록</el-button
+          >
         </div>
         <div class="list" v-if="filterDatas.length !== 0">
-          <el-card class="box-card" :class="{'_more': filterDatas.length > 7}">
-            <h2 class="list-title">이름 (총 인원: {{ filterDatas.length }} 명 )</h2>
-            <div v-for="(item, index) in filterDatas" :key="item.index" class="text item">
+          <el-card class="box-card" :class="{ _more: filterDatas.length > 7 }">
+            <h2 class="list-title">
+              이름 (총 인원: {{ filterDatas.length }} 명 )
+            </h2>
+            <div
+              v-for="(item, index) in filterDatas"
+              :key="item.index"
+              class="text item"
+            >
               {{ index + 1 }}. {{ item }}
             </div>
-            <div style="display: flex; padding-top: 30px;">
-              <el-button type="success" icon="el-icon-search" @click="start()" v-loading.fullscreen.lock="fullscreenLoading">추첨하기</el-button>
-              <el-button type="danger" icon="el-icon-delete" @click="reset()"></el-button>
+            <div style="display: flex; padding-top: 30px">
+              <el-button
+                type="success"
+                icon="el-icon-search"
+                @click="start()"
+                v-loading.fullscreen.lock="fullscreenLoading"
+                >추첨하기</el-button
+              >
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                @click="reset()"
+              ></el-button>
             </div>
           </el-card>
         </div>
       </div>
       <div class="footer">
-          <el-button type="danger" icon="el-icon-arrow-left" class="back-button" @click="backPage()">뒤로가기</el-button>
+        <el-button
+          type="danger"
+          icon="el-icon-arrow-left"
+          class="back-button"
+          @click="backPage()"
+          >뒤로가기</el-button
+        >
       </div>
     </div>
   </section>
@@ -33,84 +55,86 @@
 
 <script>
 export default {
-  name: 'PlayPage',
+  name: "PlayPage",
   data() {
     return {
-      input1: '',
+      input1: "",
       datas: [],
       filterDatas: [],
-      result: '',
+      result: "",
       fullscreenLoading: false,
-    }
+    };
   },
   computed: {
-      getTitle() {
-          return this.$route.query.title
-      }
+    getTitle() {
+      return this.$route.query.title;
+    },
   },
   mounted() {
-    this.isTitle()  
+    this.isTitle();
   },
   methods: {
     isTitle() {
-        if (!this.$route.query.title) {
-            this.$router.push('/')
-        }
+      if (!this.$route.query.title) {
+        this.$router.push("/");
+      }
     },
     backPage() {
-        this.$router.push('/')
-        localStorage.clear()
+      this.$router.push("/");
+      localStorage.clear();
     },
     async invite() {
-      if (this.input1 === '') {
-        this.$alert('이름을 입력해주세요', '알림', {
-          confirmButtonText: 'OK'
+      if (this.input1 === "") {
+        this.$alert("이름을 입력해주세요", "알림", {
+          confirmButtonText: "OK",
         });
-        return false
+        return false;
       } else {
         await this.datas.push(this.input1);
         const array = this.datas;
         this.filterDatas = [...new Set(array)];
-        this.input1 = ''
+        this.input1 = "";
       }
     },
     reset() {
-      this.$confirm('정말 삭제하시겠습니까?', '알림', {
-          confirmButtonText: '네',
-          cancelButtonText: '아니오',
-          type: 'warning'
-        }).then(() => {
+      this.$confirm("정말 삭제하시겠습니까?", "알림", {
+        confirmButtonText: "네",
+        cancelButtonText: "아니오",
+        type: "warning",
+      })
+        .then(() => {
           this.$message({
-            type: 'success',
-            message: '삭제 되었습니다.'
+            type: "success",
+            message: "삭제 되었습니다.",
           });
-          this.datas = []
-          this.filterDatas = []
-        }).catch(() => {
-        });
+          this.datas = [];
+          this.filterDatas = [];
+        })
+        .catch(() => {});
     },
     start() {
       if (this.datas.length === 1) {
-        this.$alert('2명이상 입력해주세요 !', '알림', {
-          confirmButtonText: 'OK'
+        this.$alert("2명이상 입력해주세요 !", "알림", {
+          confirmButtonText: "OK",
         });
-        return false
+        return false;
       } else {
         this.fullscreenLoading = true;
         setTimeout(() => {
-          this.result = this.filterDatas[Math.floor(Math.random() * this.filterDatas.length)]
+          this.result =
+            this.filterDatas[
+              Math.floor(Math.random() * this.filterDatas.length)
+            ];
           this.fullscreenLoading = false;
-          this.$alert(this.result, '당첨자!', {
-            confirmButtonText: 'OK',
-            callback: action => {
-              
-            }
+          this.$alert(this.result, "당첨자!", {
+            confirmButtonText: "OK",
+            callback: (action) => {},
           });
         }, 1000);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style>
 body,
@@ -162,13 +186,13 @@ body {
 }
 
 .el-input__inner {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
 }
 
 .el-button--primary {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
 }
 
 .list-title {
@@ -197,35 +221,34 @@ body {
 }
 
 .footer {
-    width: 100%;
-    position: absolute;
-    bottom: 0;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
 }
 
 .el-button--danger {
-    width: 100%;
+  width: 100%;
 }
 
 /* ===== Scrollbar CSS ===== */
-  /* Firefox */
-  * {
-    scrollbar-width: auto;
-    scrollbar-color: #e3e3e3 #ffffff;
-  }
+/* Firefox */
+* {
+  scrollbar-width: auto;
+  scrollbar-color: #e3e3e3 #ffffff;
+}
 
-  /* Chrome, Edge, and Safari */
-  *::-webkit-scrollbar {
-    width: 11px;
-  }
+/* Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 11px;
+}
 
-  *::-webkit-scrollbar-track {
-    background: #ffffff;
-  }
+*::-webkit-scrollbar-track {
+  background: #ffffff;
+}
 
-  *::-webkit-scrollbar-thumb {
-    background-color: #e3e3e3;
-    border-radius: 10px;
-    border: 1px solid #ffffff;
-  }
-
+*::-webkit-scrollbar-thumb {
+  background-color: #e3e3e3;
+  border-radius: 10px;
+  border: 1px solid #ffffff;
+}
 </style>
